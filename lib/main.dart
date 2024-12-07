fb0f49080d1f08de3bff012823b480054ece97c3
Debora-Mind <debora.almeida.de.mello@gmail.com>
2024-12-06 22:15:28 -0300

    Widgets refatorados

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/product_list.dart';
 import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/pages/products_overview_pages.dart';
 import 'package:shop/utils/app_routes.dart';
@@ -12,24 +14,27 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
-        primaryColor: Colors.purple,
-        hintColor: Colors.deepOrange,
-        useMaterial3: true,
-        appBarTheme: AppBarTheme(
-          centerTitle: true,
-          toolbarHeight: MediaQuery.of(context).size.height * 0.07,
-          foregroundColor: Colors.white,
-          backgroundColor: Colors.purple,
+    return ChangeNotifierProvider(
+      create: (_) => ProductList(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
+          primaryColor: Colors.purple,
+          hintColor: Colors.deepOrange,
+          useMaterial3: true,
+          appBarTheme: AppBarTheme(
+            centerTitle: true,
+            toolbarHeight: MediaQuery.of(context).size.height * 0.07,
+            foregroundColor: Colors.white,
+            backgroundColor: Colors.purple,
+          ),
+          fontFamily: 'Lato',
         ),
-        fontFamily: 'Lato',
+        home: const ProductsOverviewPages(),
+        routes: {AppRoutes.PRODUCT_DETAIL: (ctx) => const ProductDetailPage()},
+        debugShowCheckedModeBanner: false,
       ),
-      home: ProductsOverviewPages(),
-      routes: {AppRoutes.PRODUCT_DETAIL: (ctx) => const ProductDetailPage()},
-      debugShowCheckedModeBanner: false,
     );
   }
 }
--- lib/pages/products_overview_pages.dart
+++ lib/pages/products_overview_pages.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:shop/components/product_item.dart';
-import 'package:shop/data/dummy_data.dart';
-import 'package:shop/models/product.dart';
+import 'package:shop/components/product_grid.dart';
 
 class ProductsOverviewPages extends StatelessWidget {
-  final List<Product> loadedProducts = dummyProducts;
-
-  ProductsOverviewPages({super.key});
+  const ProductsOverviewPages({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -14,17 +10,7 @@ class ProductsOverviewPages extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Minha Loja'),
       ),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(10),
-        itemCount: loadedProducts.length,
-        itemBuilder: (ctx, i) => ProductItem(product: loadedProducts[i]),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-      ),
+      body: const ProductGrid(),
     );
   }
 }
